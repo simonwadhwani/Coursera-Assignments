e6b0574e1accead56726026292a7c7ab598551f6
simonwadhwani <99025357+simonwadhwani@users.noreply.github.com>
2022-02-20 16:57:36 +0530

    Add files via upload

---
+++ module4-solution/Hello.js
@@ -0,0 +1,13 @@
+(function () {
+
+	var helloSpeaker = {};
+	
+    var speakWord = "Hello";
+
+	helloSpeaker.speak = function speak(name) {
+  console.log(speakWord + " " + name);
+}
+
+window.helloSpeaker = helloSpeaker;
+
+})();
